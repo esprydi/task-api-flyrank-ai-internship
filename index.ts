ced3d97ae685bda907747997a30020b13b85a8e6
Endang Supriyadi <supriyadiendang379@gmail.com>
2026-07-31 23:38:27 +0700

    Stage 1: root and health endpoints

--- index.ts
+++ index.ts
@@ -5,8 +5,18 @@ const port= 3000;
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello Server");
+    res.json({
+        name: "Task API",
+        version: "1.0",
+        endpoint: ["/task"]
+    });
 });
+
+app.get("/health", (req: Request, res: Response) => {
+    res.json({
+        status: "ok"
+    })
+})
  
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
