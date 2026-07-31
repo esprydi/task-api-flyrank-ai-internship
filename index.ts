3841b40d501b2e067e061b61c1676c0f6e7ae1f6
Endang Supriyadi <supriyadiendang379@gmail.com>
2026-07-31 23:46:34 +0700

    Stage 2: read endpoints with 404

--- index.ts
+++ index.ts
@@ -1,5 +1,25 @@
 import express, { Request, Response } from 'express';
+import console = require('node:console');
 const app = express();
+interface Task {
+    id: number;
+    title: string;
+    done: boolean;
+}
+
+let tasks: Task[] = [
+    {
+        id: 1,
+        title: "Task 1",
+        done: false
+    },
+    {
+        id: 2,
+        title: "Task 2",
+        done: false
+    }
+]
+
 const port= 3000;
 
 app.use(express.json());
@@ -17,6 +37,22 @@ app.get("/health", (req: Request, res: Response) => {
         status: "ok"
     })
 })
+
+app.get('/tasks', (req: Request, res: Response) => {
+    res.json(tasks);
+})
+
+app.get('/tasks/:id', (req: Request, res: Response) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(task => task.id === taskId);
+    if (task) {
+        res.json(task);
+    } else {
+        res.status(404).json({ error: `Task with ID ${taskId} not found`});
+    }
+})
+
+
  
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
